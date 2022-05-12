8daf9dd1c472c994df1060596aa97d517bdff42f
hukemo <laurins.ding@web.de>
2022-05-12 12:34:18 +0200

    neues Projekt

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let Punkte;
+let neuePunktzahl;
+let summeSpieler1 = 0;
+let summeSpieler2 = 0;
+let anzahlWuerfe1 = 0;
+let anzahlWuerfe2 = 0;
+
+function wuerfeln(){
+let min = 2;
+let max = 12;
+let x = Math.floor(Math.random()*(max-min)+min);
+console.log(x);
+}
